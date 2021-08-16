610a803ec1f3c17aa8234878679b6b3e935ebed2
gidwikintan <gidwikintan@gmail.com>
2021-08-16 22:37:32 +0700

    fix overlay animations

--- app/js/script.js
+++ app/js/script.js
@@ -1,16 +1,21 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
-header.addEventListener('click', function(){
+btnHamburger.addEventListener('click', function(){
     
     if(header.classList.contains('open')){
         header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
     }else{
         header.classList.add('open');
-        overlay.classList.remove('fade-out')
-        overlay.classList.add('fade-in');
+        fadeElems.forEach(function(element){
+        element.classList.remove('fade-out')
+        element.classList.add('fade-in');
+        })
     }
 })
